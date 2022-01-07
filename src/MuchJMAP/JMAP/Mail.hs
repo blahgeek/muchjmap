e28aa2778cc0a52b0af1a252f6642741fda7c71d
Yikai Zhao <yikai@z1k.dev>
2022-01-07 23:28:38 +0800

    Get mail

--- src/MuchJMAP/JMAP/Mail.hs
+++ src/MuchJMAP/JMAP/Mail.hs
@@ -7,7 +7,8 @@ module MuchJMAP.JMAP.Mail where
 import GHC.Generics
 import qualified Data.Aeson as Aeson
 import Data.Char (toLower)
-import qualified Data.Map as Map
+import Data.Map (Map)
+import qualified Data.Text as Text
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Catch (MonadThrow)
 
@@ -26,7 +27,10 @@ import MuchJMAP.JMAP.API ( RequestContext
                          , parseResponseBody0)
 
 newtype MailboxId = MailboxId String
-  deriving (Aeson.ToJSON, Aeson.FromJSON, Show)
+  deriving (Aeson.ToJSON, Aeson.FromJSON, Show, Aeson.FromJSONKey, Eq)
+
+instance Ord MailboxId where
+  compare (MailboxId a) (MailboxId b) = compare a b
 
 data WellknownMailboxRole = RoleAll | RoleArchive | RoleDrafts | RoleFlagged |
                             RoleImportant | RoleInbox | RoleJunk | RoleSent |
@@ -66,3 +70,39 @@ getAllMailbox (config, session) = do
                          , methodCallArgs = args }
         args = methodCallArgsFrom
           [("accountId", methodCallArgFrom $ getPrimaryAccount session MailCapability)]
+
+
+newtype EmailId = EmailId String
+  deriving (Aeson.ToJSON, Aeson.FromJSON, Show)
+
+newtype BlobId = BlobId String
+  deriving (Aeson.ToJSON, Aeson.FromJSON, Show)
+
+data Email = Email { emailId :: EmailId
+                   , emailBlobId :: BlobId
+                   , emailMailboxIds :: Map MailboxId Bool
+                   , emailKeywords :: Map String Bool  -- TODO
+                   , emailSize :: Int
+                   , emailSubject :: Text.Text}
+             deriving (Show, Generic)
+
+instance Aeson.FromJSON Email where
+  parseJSON = Aeson.genericParseJSON $ aesonOptionWithLabelPrefix "email"
+
+getAllEmail :: (MonadIO m, MonadThrow m) => RequestContext -> m [Email]
+getAllEmail (config, session) = do
+  api_response <- apiRequest (config, session) (Request [query_call, get_call])
+  method_response <- parseResponseBody0 "get_call" api_response
+  return $ getResponseList method_response
+  where query_call = MethodCall { methodCallCapability = MailCapability
+                                , methodCallName = "Email/query"
+                                , methodCallId = "query_call"
+                                , methodCallArgs = methodCallArgsFrom query_args}
+        query_args = [ ("accountId", methodCallArgFrom $ getPrimaryAccount session MailCapability)
+                     , ("limit", methodCallArgFrom (10 :: Int))]
+        get_call = MethodCall { methodCallCapability = MailCapability
+                              , methodCallName = "Email/get"
+                              , methodCallId = "get_call"
+                              , methodCallArgs = methodCallArgsFrom get_args}
+        get_args = [ ("accountId", methodCallArgFrom $ getPrimaryAccount session MailCapability)
+                   , ("ids", ResultReference query_call "/ids")]
