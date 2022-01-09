bbc39d3c1340ed556afe400059d43b32fd49a1c0
Yikai Zhao <yikai@z1k.dev>
2022-01-09 10:46:45 +0800

    Add cmdargs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
 
-import Lib
+import qualified Network.JMAP.API as JMAPAPI
+import qualified MuchJMAP.App as App
+
+import System.Console.CmdArgs
+import qualified Data.ByteString.Char8 as C
+import Data.Data (Data, Typeable)
+import Data.Maybe
+import qualified Data.Aeson as Aeson
+
+data ConfigPath = ConfigPath { configPath :: FilePath }
+  deriving (Show, Data, Typeable)
+
+configPathArg = ConfigPath { configPath = def}
 
 main :: IO ()
-main = someFunc
+main = do
+  config_path <- cmdArgs configPathArg
+  conf <- Aeson.decodeFileStrict' $ configPath config_path
+  session <- JMAPAPI.getSessionResource (fromJust conf)
+  print session
+  emails <- App.getAllEmail (fromJust conf, session)
+  print emails
